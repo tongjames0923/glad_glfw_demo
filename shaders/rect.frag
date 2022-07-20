36ec1b4a0402d1b0abfe8f5d6ef27b6efcf9fe9a
Abstergo <tongjames@live.com>
2022-07-20 09:39:03 +0800

    unform color

--- shaders/rect.frag
+++ shaders/rect.frag
@@ -3,7 +3,6 @@
 layout (location = 0) in vec2 aPos;
 layout (location = 1)in vec3 color;
 out vec3 dcolor;
-
 void main()
 {
 	gl_Position = vec4(aPos,0, 1.0);
@@ -16,5 +15,6 @@ out vec4 FragColor;
 uniform vec3 mycolor;
 void main()
 {
-	FragColor = vec4(dcolor,0);
+	FragColor = vec4(mycolor,0);
+	//mycolor=dcolor;
 }
--- source/practice.cpp
+++ source/practice.cpp
@@ -1,9 +1,10 @@
 #include "practice.h"
 string shader_path = "../shaders/rect.frag";
 float vers[20];
+Shader st;
 int rect(GLFWwindow *w)
 {
-
+    glUniform3f(glGetUniformLocation(st.getId(), "mycolor"), 1, 0, 0);
     // glBufferData(GL_ARRAY_BUFFER, 20 * sizeof(float), vers, GL_STATIC_DRAW);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     return GLWindow::DRAW_CONTINUTE;
@@ -39,12 +40,12 @@ void drawRect(GLWindow *window, float w, float h, float x, float y, float r, flo
     unsigned int indexs[]{
         0, 1, 2,
         1, 2, 3};
-    Shader s(shader_path);
-    s.GenVertexArray();
-    s.makeBufferAndBind("VER", temps, sizeof(float), 20, GL_ARRAY_BUFFER);
-    s.makeBufferAndBind("IND", indexs, sizeof(unsigned int), 6, GL_ELEMENT_ARRAY_BUFFER);
-    s.enableVertexBuffer(2, 5, 0, 0);
-    s.enableVertexBuffer(3, 5, 1, 2);
-    window->useShader(s);
+    st.input(shader_path);
+    st.GenVertexArray();
+    st.makeBufferAndBind("VER", temps, sizeof(float), 20, GL_ARRAY_BUFFER);
+    st.makeBufferAndBind("IND", indexs, sizeof(unsigned int), 6, GL_ELEMENT_ARRAY_BUFFER);
+    st.enableVertexBuffer(2, 5, 0, 0);
+    st.enableVertexBuffer(3, 5, 1, 2);
+    window->useShader(st);
     window->draw(rect);
 }

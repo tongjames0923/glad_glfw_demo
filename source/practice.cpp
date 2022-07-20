894edc97d5043339fa0c22afb153f1c1af273450
Abstergo <tongjames@live.com>
2022-07-20 09:20:58 +0800

    调整调用灵活性。使用glbufferdata可以动态修改绘制

--- source/practice.cpp
+++ source/practice.cpp
@@ -1,14 +1,16 @@
 #include "practice.h"
 string shader_path = "../shaders/rect.frag";
+float vers[20];
 int rect(GLFWwindow *w)
 {
+
+    // glBufferData(GL_ARRAY_BUFFER, 20 * sizeof(float), vers, GL_STATIC_DRAW);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     return GLWindow::DRAW_CONTINUTE;
 }
 void drawRect(GLWindow *window, float w, float h, float x, float y, float r, float g, float b)
 {
-    Shader s(shader_path);
-    float ver[] = {
+    float temps[] = {
         x,
         y,
         r,
@@ -30,11 +32,16 @@ void drawRect(GLWindow *window, float w, float h, float x, float y, float r, flo
         g,
         b,
     };
+    for (int i = 0; i < 20; i++)
+    {
+        vers[i] = temps[i] / 2;
+    }
     unsigned int indexs[]{
         0, 1, 2,
         1, 2, 3};
+    Shader s(shader_path);
     s.GenVertexArray();
-    s.makeBufferAndBind("VER", ver, sizeof(float), 20, GL_ARRAY_BUFFER);
+    s.makeBufferAndBind("VER", temps, sizeof(float), 20, GL_ARRAY_BUFFER);
     s.makeBufferAndBind("IND", indexs, sizeof(unsigned int), 6, GL_ELEMENT_ARRAY_BUFFER);
     s.enableVertexBuffer(2, 5, 0, 0);
     s.enableVertexBuffer(3, 5, 1, 2);
